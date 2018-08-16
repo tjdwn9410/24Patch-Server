275fa6e1943d98f06c57db803e8cc185108479f5
Ubuntu <ubuntu@ip-172-31-22-213.ap-northeast-2.compute.internal>
2018-08-16 09:31:15 +0000

    Login add

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 let jwtUtil = require('../util/jwt_util')
 let dbPool = require('../util/db_util')
-/* GET home page. */
 
+// 회원가입
 router.post('/signup', async function(req, res, next) {
   const {
     id,
@@ -45,9 +45,46 @@ router.post('/signup', async function(req, res, next) {
     })
   }
 });
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// 로그인
+router.post('/signin', async function(req, res, next) {
+  const {
+    id,
+    password
+  } = req.body;
+  try {
+    const connection = await dbPool.getConnection();
+    try {
+      const result = await connection.query('SELECT id FROM users WHERE id=? and password = password(?)', [id, password]);
+      connection.release();
+      if(result[0].length === 1) {
+        const token = jwtUtil.tokenGenerator({
+          "id": id
+        });
+        res.status(200);
+        res.json({
+          'msg': 'Success',
+          'token': token
+        });
+      } else {
+        res.status(401);
+        res.json({
+          'msg': 'Invalid Id or Password'
+        });
+      }
+    } catch (err) {
+      connection.release();
+      console.log(err);
+    }
+
+  } catch (err) {
+    connection.release();
+    res.status(500);
+    res.json({
+      'msg': 'Server Error'
+    })
+    console.log(err);
+  }
 });
 
 module.exports = router;
