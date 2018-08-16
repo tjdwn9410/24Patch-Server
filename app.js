099b2a2f1651b2a0806ba02c638a858d76d68984
Ubuntu <ubuntu@ip-172-31-22-213.ap-northeast-2.compute.internal>
2018-08-16 09:10:00 +0000

    Signup add

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
+const mysql = require('mysql');
+
+const config = require('./config');
+const connection = mysql.createConnection(config.dbConnInfo);
+connection.connect();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+let jwtUtil = require('../util/jwt_util')
+let dbPool = require('../util/db_util')
+/* GET home page. */
+
+router.post('/signup', async function(req, res, next) {
+  const {
+    id,
+    password,
+    age,
+    gender,
+    height,
+    weight
+  } = req.body;
+  try {
+    const connection = await dbPool.getConnection();
+    try {
+      const result = await connection.query('INSERT INTO users(id, password, age, gender, height, weight) VALUES(?,password(?),?,?,?,?)', [id, password, age, gender, height, weight]);
+      connection.release();
+      const token = jwtUtil.tokenGenerator({
+        "id": id
+      });
+      res.status(200);
+      res.json({
+        'msg': 'Success',
+        'token': token
+      });
+    } catch (err) {
+      connection.release();
+      if (err.code === 'ER_DUP_ENTRY') {
+        res.status(409);
+        res.json({
+          'msg': 'Duplicated Id'
+        })
+      } else {}
+    }
+
+  } catch (err) {
+    connection.release();
+    res.status(500);
+    res.json({
+      'msg': 'Server Error'
+    })
+  }
+});
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
